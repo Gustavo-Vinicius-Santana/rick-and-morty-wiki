0c2e20c72fa398e4bdc1f0db9e2a2cb3caafbc55
Gustavo Vinicius <gus.tec.dev@gmail.com>
2025-07-31 13:17:55 -0300

    (feat): adicionando paginação na tela de personagem

--- src/app/personagens/page.tsx
+++ src/app/personagens/page.tsx
@@ -1,26 +1,31 @@
 "use client";
+import { useState } from "react";
 import { useListPersonagens } from "@/lib/api/hooks/usePersonagens";
 import CardPersonagem from "@/ui/components/cards/cardPersonagem";
+import Paginacao from "@/ui/components/pagination/paginacao";
 
-export default function page() {
-    const {data, isLoading, isError} = useListPersonagens(1);
+export default function Page() {
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isError } = useListPersonagens(page);
 
-    if (isLoading) return <div>Loading...</div>;
-    if (isError) return <div>Error</div>;
+  if (isLoading) return <div className="text-center py-20">Carregando...</div>;
+  if (isError) return <div className="text-center text-red-500">Erro ao carregar personagens.</div>;
 
-    console.log(data);
+  const totalPages = data.info.pages;
 
-    return (
-        <div className="max-w-6xl mx-auto px-4 py-10">
-            <h1 className="text-3xl font-bold mb-8 text-center text-gray-800">
-                Personagens
-            </h1>
+  return (
+    <div className="max-w-6xl mx-auto px-4 py-10">
+      <h1 className="text-3xl font-bold mb-8 text-center text-gray-800">Personagens</h1>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center">
-                {data.results.map((personagem: any) => (
-                <CardPersonagem key={personagem.id} image={personagem.image} name={personagem.name} />
-                ))}
-            </div>
+        <Paginacao page={page} setPage={setPage} totalPages={totalPages} hasNextPage={data.info.next} />
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center mb-8 mt-8">
+        {data.results.map((personagem: any) => (
+            <CardPersonagem key={personagem.id} image={personagem.image} name={personagem.name} />
+        ))}
         </div>
-    )
+
+        <Paginacao page={page} setPage={setPage} totalPages={totalPages} hasNextPage={data.info.next} />
+    </div>
+  );
 }
--- src/lib/api/services/personagemService.ts
+++ src/lib/api/services/personagemService.ts
@@ -1,7 +1,7 @@
 import { API } from "../api";
 
 export async function getCharacters(page = 1) {
-  const response = await API.get(`/character?page=${page}`);
+  const response = await API.get(`/character/?page=${page}`);
   return response.data;
 }
 
