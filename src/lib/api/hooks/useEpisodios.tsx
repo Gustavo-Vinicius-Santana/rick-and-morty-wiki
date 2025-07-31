d3942d758e55648d4fa2cce972364091b9f36206
Gustavo Vinicius <gus.tec.dev@gmail.com>
2025-07-31 13:57:05 -0300

    (feat): listando todos os episodios e organizando-os por temporada

--- src/lib/api/hooks/useEpisodios.tsx
+++ src/lib/api/hooks/useEpisodios.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getEpisodes } from "../services/episodiosService";
+import { getEpisodes, getAllEpisodes } from "../services/episodiosService";
 
 export const useListEpisodios = (page: number) => {
     return useQuery({
@@ -7,3 +7,11 @@ export const useListEpisodios = (page: number) => {
         queryFn: () => getEpisodes(page),
     })
 };
+
+export const useListAllEpisodios = () => {
+    return(useQuery({
+        queryKey: ['allEpisodios'],
+        queryFn: () => getAllEpisodes(),
+        staleTime: 1000 * 60 * 5
+    }))
+}
--- src/lib/api/services/episodiosService.ts
+++ src/lib/api/services/episodiosService.ts
@@ -4,3 +4,21 @@ export async function getEpisodes(page = 1) {
     const response = await API.get(`/episode?page=${page}`);
     return response.data;
 }
+
+export async function getAllEpisodes() {
+  let allEpisodes: any[] = [];
+  let page = 1;
+  let hasNext = true;
+
+  while (hasNext) {
+    const response = await API.get(`/episode?page=${page}`);
+    const data = response.data;
+
+    allEpisodes.push(...data.results);
+
+    hasNext = !!data.info.next;
+    page++;
+  }
+
+  return allEpisodes;
+}
