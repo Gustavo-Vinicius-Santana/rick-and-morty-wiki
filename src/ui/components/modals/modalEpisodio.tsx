0d96c315aea0a084354b43f110f37d24bddc305a
Gustavo Vinicius <gus.tec.dev@gmail.com>
2025-08-01 19:15:06 -0300

    (feat): listando todos os persoganes do modal de episodios

--- src/ui/components/modals/modalEpisodio.tsx
+++ src/ui/components/modals/modalEpisodio.tsx
@@ -6,29 +6,47 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/ui/shadcn/components/dialog";
+import { ScrollArea } from "@/ui/shadcn/components/scroll-area";
 
 import { useModalEpisodioStore } from "@/lib/stores/modalStore";
 import { useListEspecificEpisodio } from "@/lib/api/hooks/useEpisodios";
+import { useListManyPersonagens } from "@/lib/api/hooks/usePersonagens";
+import CardPersonagem from "@/ui/components/cards/cardPersonagem";
 
 export default function ModalEpisodio() {
   const { isOpen, id, onClose } = useModalEpisodioStore();
 
   const { data, isLoading, isError } = useListEspecificEpisodio(id);
 
+  const characterIds = (data?.characters || []).map((url: string) =>
+    Number(url.split("/").pop())
+  );
+
+  const {
+    data: personagens,
+    isLoading: isLoadingPersonagens,
+    isError: isErrorPersonagens,
+  } = useListManyPersonagens(characterIds);
+
   if (!isOpen || !id) return null;
 
-  if (isLoading) return <div className="text-center py-20">Carregando...</div>;
+  if (isLoading || isLoadingPersonagens)
+    return <div className="text-center py-20">Carregando...</div>;
 
-  if (isError || !data)
+  if (isError || !data || isErrorPersonagens || !personagens)
     return (
       <div className="text-center text-red-500">
-        Erro ao carregar episódio.
+        Erro ao carregar episódio ou personagens.
       </div>
     );
 
+  const listaPersonagens = Array.isArray(personagens)
+    ? personagens
+    : [personagens];
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="max-w-md">
+      <DialogContent className="max-w-md max-h-[80vh] overflow-hidden">
         <DialogHeader>
           <DialogTitle className="text-xl font-bold">{data.name}</DialogTitle>
         </DialogHeader>
@@ -40,18 +58,22 @@ export default function ModalEpisodio() {
           <p>
             <strong>Episódio:</strong> {data.episode}
           </p>
-          <p>
-            <strong>Personagens:</strong>{" "}
-            {(data.characters as string[])
-              .slice(0, 5)
-              .map((charUrl, index) => (
-                <span key={charUrl}>
-                  {index > 0 && ", "}
-                  {`#${charUrl.split("/").pop()}`}
-                </span>
-              ))}
-            {data.characters.length > 5 && " ..."}
-          </p>
+
+          <div>
+            <strong>Personagens:</strong>
+            <ScrollArea className="mt-2 h-64 pr-2">
+              <div className="flex flex-col items-center justify-start space-y-2">
+                {listaPersonagens.map((personagem) => (
+                  <CardPersonagem
+                    key={personagem.id}
+                    id={personagem.id}
+                    image={personagem.image}
+                    name={personagem.name}
+                  />
+                ))}
+              </div>
+            </ScrollArea>
+          </div>
         </div>
       </DialogContent>
     </Dialog>
